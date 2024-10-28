5569ddae78acf044b5c8e0526adec2014f78afc4
Carlos Dominguez Puig <carlos.dom13@hotmail.com>
2024-10-29 00:22:21 +0100

    Crear mensaje ordenados

---
+++ src/appWriteConfig.js
@@ -0,0 +1,10 @@
+import { Client, Databases } from 'appwrite';
+
+export const PROJECT_ID = '671fa7450021c9ae4dc7';
+export const DATABASE_ID = '67200f91000d5b573df6';
+export const COLLECTION_ID_MESSAGES = '67200f9e0021e8df13aa';
+const client = new Client();
+client.setProject('671fa7450021c9ae4dc7');
+
+export const database = new Databases(client);
+export default client;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import {COLLECTION_ID_MESSAGES, database, DATABASE_ID} from "../appWriteConfig.js";
+import {ID, Query} from 'appwrite'
 
 const Room = () => {
-  return (
-    <div>Room</div>
-  )
+    const [messages, setMessages] = useState([]);
+    const [messageBody, setMessageBody] = useState("");
+    const getMessages = async () => {
+        const response = await database.listDocuments(
+            DATABASE_ID,
+            COLLECTION_ID_MESSAGES, [
+                Query.orderDesc('$createdAt')
+            ])
+        console.log(response);
+        setMessages(response.documents)
+    }
+    useEffect(() => {
+        getMessages();
+    }, []);
+    const handleSumit = async (e) => {
+        e.preventDefault();
+        let payload = {
+            body: messageBody
+        }
+        let response = await database.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID_MESSAGES,
+            ID.unique(),
+            payload
+        );
+        setMessages(prevState => [response, ...messages]);
+        setMessageBody("");
+    }
+    return (
+        <main className="container">
+            <div className="room--container">
+                <form onSubmit={handleSumit} id="message--form">
+                    <div>
+                        <textarea
+                            required
+                            maxLength="1000"
+                            placeholder="Say algo"
+                            onChange={(e)=>{setMessageBody(e.target.value)}}
+                            value={messageBody}>
+                        </textarea>
+                    </div>
+                    <div className="send-btn--wrapper">
+                        <input className="btn btn--secondary" type="submit" value="Send"/>
+                    </div>
+                </form>
+                <div>
+                    {messages && messages.map((message) => (
+                        <div key={message.$id} className="message--wrapper">
+                            <div className="message--header">
+                                <small className="message-timestamp">{message.$createdAt}</small>
+                            </div>
+                            <div className="message--body">
+                                <span>{message.body}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Room
