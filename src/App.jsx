0facfc230ae82b946cab7b49dd88523595df1c1e
Carlos Dominguez <carlos.dom13@hotmail.com>
2024-10-28 16:06:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Room from './pages/Room'
+
+function App() {
+  return (
+    <>
+      <Room></Room>
+    </>
+  )
+}
+
+export default App
