7553ed71f122399e003e7626c16668eb338eb330
Carlos Dominguez Puig <carlos.dom13@hotmail.com>
2024-10-29 09:20:50 +0100

    Crear mensaje realtime

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react'
-import {COLLECTION_ID_MESSAGES, database, DATABASE_ID} from "../appWriteConfig.js";
+import client, {COLLECTION_ID_MESSAGES, database, DATABASE_ID} from "../appWriteConfig.js";
 import {ID, Query} from 'appwrite'
+import {Trash2} from "react-feather";
 
 const Room = () => {
     const [messages, setMessages] = useState([]);
@@ -9,27 +10,46 @@ const Room = () => {
         const response = await database.listDocuments(
             DATABASE_ID,
             COLLECTION_ID_MESSAGES, [
-                Query.orderDesc('$createdAt')
+                Query.orderDesc('$createdAt'),
+                Query.limit(10)
             ])
         console.log(response);
         setMessages(response.documents)
     }
     useEffect(() => {
         getMessages();
+        const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGES}.documents`, (response) => {
+            console.log(response);
+            if (response.events.some(event => /databases\.\*\.collections\.\*\.documents\.\*\.create/.test(event))){
+                setMessages(prevState => [response.payload, ...prevState]);
+                console.log("create");
+            }
+            if(response.events.some(event => /databases\.\*\.collections\.\*\.documents\.\*\.delete/.test(event))){
+                setMessages(prevState => [response.payload, ...prevState]);
+                console.log("delete");
+            }
+        });
+        return () => {
+            unsubscribe();
+        }
     }, []);
     const handleSumit = async (e) => {
         e.preventDefault();
         let payload = {
             body: messageBody
         }
-        let response = await database.createDocument(
+        await database.createDocument(
             DATABASE_ID,
             COLLECTION_ID_MESSAGES,
             ID.unique(),
             payload
         );
-        setMessages(prevState => [response, ...messages]);
         setMessageBody("");
+    }
+    const handleDelete = async (id) => {
+        console.log(id);
+        await database.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGES, id);
+        messages.filter((message) => message.$id !== id);
     }
     return (
         <main className="container">
@@ -52,7 +72,10 @@ const Room = () => {
                     {messages && messages.map((message) => (
                         <div key={message.$id} className="message--wrapper">
                             <div className="message--header">
-                                <small className="message-timestamp">{message.$createdAt}</small>
+                                <small className="message-timestamp">
+                                    {new Date(message.$createdAt).toLocaleDateString()} {new Date(message.$createdAt).toLocaleTimeString()}
+                                </small>
+                                <Trash2 className="delete--btn" onClick={() => handleDelete(message.$id)}>Delete</Trash2>
                             </div>
                             <div className="message--body">
                                 <span>{message.body}</span>
